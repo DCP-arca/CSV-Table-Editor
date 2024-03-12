2eb7290ad4645139b8450daa0524e5f66daf1615
DCP-arce <127241088+DCP-arca@users.noreply.github.com>
2024-03-13 02:50:04 +0900

    빌더 파일 이름 변경

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
-cd D:\Dev\Workspace\Python\CSV-Table-Editor
-
-D:\Dev\Workspace\Python\CSV-Table-Editor\.venv\Scripts\activate.bat
+call D:\Dev\Workspace\Python\CSV-Table-Editor\.venv\Scripts\activate.bat
 
 pyinstaller --noconfirm --windowed --hidden-import=fastparquet --hidden-import=dbfread --hidden-import=dbf --icon "D:\Dev\Workspace\Python\CSV-Table-Editor\icon.ico" --name "CSV Table Editor" "D:\Dev\Workspace\Python\CSV-Table-Editor\gui.py"
